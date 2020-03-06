230db0f42bd9fca666a4c85262bddf73f2728377
Nifemi <abioseimran@gmail.com>
2020-03-06 12:07:28 +0100

    adding files

---
+++ west/js/help.js
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/materialize.css">
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="style.css">
+   
+</head>
+<body>
+    <!-- the nav bar goes here -->
+    <nav class="white"> 
+        <div class=" nav-wrapper container">
+          <a href="#" class="brand-logo purple-text">Stories</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a class="purple-text" href="index.html">Home</a></li>
+            <li><a class="purple-text" href="team.html">Teams</a></li>
+            <li><a class="purple-text" href="#">Components</a></li>
+            <li><a class="purple-text" href="#">About</a></li>
+          </ul>
+        </div>
+      </nav>
+      
+      <!-- sub episodes in this story -->
+      <h3 class="center blue-text">SevenTeen</h3>
+      <br><br>
+      <p class="container flow-text">There are two parts in this stories
+          <br>Read and enjoy!</p>
+      <br><br>
+       <!-- the part 1 goes here-->
+       <div class="container">
+        <ul class="collapsible">
+            <li>
+              <div class="collapsible-header"><i class="material-icons">book</i>Part One</div>
+              <div class="collapsible-body"><span>Part 1 <br> </span></div>
+            </li>
+     <!-- part two of 17 goes here -->
+            <li>
+                <div class="collapsible-header"><i class="material-icons">book</i>Part Two</div>
+                <div class="collapsible-body"><span>Part 2</span></div>
+              </li>
+  
+
+
+        <!-- javascript goes here -->
+     <script
+     src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
